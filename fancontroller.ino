b1f81d75c13394048e827e378d9063ce50a634ff
Warren MacEvoy <wmacevoy@gmail.com>
2015-07-15 04:01:25 -0600

    factor

--- fancontroller.ino
+++ fancontroller.ino
@@ -4,47 +4,72 @@
 //     to the current temperature defined by the
 //     rombus below.
 //
+//     setup - initial duty is along the A-B diagonal.
+//     loop  - duty is shifted down/up on the left/right
+//             side just enough to stay in the rombus.
+//     
+//
 //     ^ duty
 //     | 
 //     |
 //     |
 //     |
 //     |              
-//     |           |<---DT--->|     D=TEMP_DELTA
-//     |           A'---------A     A=(TEMP_MAX,DUTY_MAX)
-//     |          /          /
-//     |         /          /
-//     |        /          /
-//     |       /          /
-//     |      B----------B'         B=(TEMP_MIN,DUTY_MIN)
+//     |           |<---D--->|     D=TEMP_DELTA
+//     |           B'--------B---- B=(TEMP_MAX,DUTY_MAX)
+//     |          /         /
+//     |         /         /
+//     |        /         /
+//     |       /         /
+//     |  ----A---------A'         A=(TEMP_MIN,DUTY_MIN)
 //     |
 // <---+-------------------------->
 //     |                       temp
 //     v
 
 #include "dmath.h"
-#include "GetTemp.h"
-#include "UpdateFan.h"
+#include "Temp.h"
+#include "Fan.h"
 
 const double TEMP_MIN =  15.0;
 const double TEMP_MAX = 25.0;
 const double TEMP_DELTA = 3.0;
 const double DUTY_MIN = 0.0;
 const double DUTY_MAX = 1.0;
+const unsigned long BAUD = 9600L;
 
 double temp;
 double duty;
 
 void setup()
 {
-    Serial.begin(9600);
-    temp = GetTemp();
-    duty = dconstrainmap(temp,TEMP_MIN,TEMP_MAX,DUTY_MIN,DUTY_MAX);
+  temp = TempReadC();
+  duty = dconstrainmap(temp,TEMP_MIN,TEMP_MAX,DUTY_MIN,DUTY_MAX);
+
+  if (BAUD > 0) {
+    Serial.begin(BAUD);
+    Serial.print("setup:");
+    Serial.print(" temp MIN/MAX/DELTA/value="); 
+    Serial.print(TEMP_MIN,1);
+    Serial.print("/");
+    Serial.print(TEMP_MAX,1);
+    Serial.print("/");
+    Serial.print(TEMP_DELTA,1);
+    Serial.print("/");
+    Serial.print(temp,1);
+    Serial.print(" delta MIN/MAX/value=");
+    Serial.println(DUTY_MIN,2);
+    Serial.print("/");
+    Serial.println(DUTY_MAX,2);
+    Serial.print("/");
+    Serial.print(duty,2);
+    Serial.println();
+  }
 }
 
 void loop()
 {
-  temp = GetTemp();
+  temp = TempReadC();
 
   double duty0 = dconstrainmap(temp,
 			       TEMP_MIN,TEMP_MAX-TEMP_DELTA,
@@ -57,12 +82,18 @@ void loop()
   duty = dconstrain(duty,duty0,duty1);
 
   UpdateFan(duty);
-  
-  Serial.print("temp="); Serial.print(temp);
-  Serial.print(" duty0="); Serial.print(duty0);
-  Serial.print(" duty1="); Serial.print(duty1);
-  Serial.print(" duty="); Serial.print(duty);
-  Serial.println();
-  
+
+  if (BAUD > 0) {
+    Serial.print("loop:");
+    Serial.print(" temp="); Serial.print(temp);
+    Serial.print(" duty min/max/value="); 
+    Serial.print(duty0);
+    Serial.print("/");
+    Serial.print(duty1);
+    Serial.print("/");
+    Serial.print(duty);
+    Serial.println();
+  }
+
   delay(1000);
 }
